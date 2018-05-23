70e635f8ebf4def1cc97561742e0a5ca1eeb6981
DreamWalkerT <39514978+DreamWalkerT@users.noreply.github.com>
2018-05-23 19:06:16 +0900

    敵を作り始めた

---
+++ ShiningRoadProject/SourceCodo/EnemyRobo.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include"RoboCommand.h"
+
+
+
+class clsEnemyRobo : public clsRobo
+{
+public:
+	clsEnemyRobo();
+	~clsEnemyRobo();
+
+private:
+
+};
+
+clsEnemyRobo::clsEnemyRobo()
+{
+}
+
+clsEnemyRobo::~clsEnemyRobo()
+{
+}
