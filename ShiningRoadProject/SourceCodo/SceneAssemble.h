50830ceb9b92d6a6aed60d868d0fbeb2c9d5054f
Tone2387 <34734345+Tone2387@users.noreply.github.com>
2018-06-06 14:26:22 +0900

    アセンブルにファイルクラス追加

--- ShiningRoadProject/SourceCodo/SceneAssemble.h
+++ ShiningRoadProject/SourceCodo/SceneAssemble.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "File.h"
+
 #include "SceneClass.h"
 #include "CameraAssemble.h"
 #include "AssembleModel.h"
 
 #include <vector>
+#include <string>
 
 //================================//
 //========== 組み換えクラス ==========//
@@ -14,6 +17,31 @@ class clsSCENE_ASSEMBLE : public clsSCENE_BASE
 public:
 	clsSCENE_ASSEMBLE( clsPOINTER_GROUP* const ptrGroup );
 	~clsSCENE_ASSEMBLE();
+
+	//選択肢のあるパーツの種類( 配列の添え字になる ).
+	enum enPARTS_TYPES : UCHAR
+	{
+		LEG = 0,
+		CORE,
+		HEAD,
+		ARMS,
+		WEAPON,
+
+		ENUM_SIZE
+	};
+
+	//どのパーツを選んでるの?.
+	struct PARTS_SELECT
+	{
+		int iType;	//パーツの種類( 脚、コア等 ).
+		int iNum;	//パーツ番号.
+
+		PARTS_SELECT(){
+			iType = iNum = 0;
+		}
+	}m_PartsSelect;
+
+
 
 private:
 
@@ -29,6 +57,7 @@ private:
 
 	clsASSEMBLE_MODEL*	m_pAsmModel;
 
+	clsFILE*	m_pFile[enPARTS_TYPES::ENUM_SIZE];
 
 //	clsSPRITE2D_CENTER* m_pSprite;
 //	clsCharaStatic* m_pTestChara;
