5b2198f4cf020325c7997fc56461f41929bb6540
DreamWalkerT <39514978+DreamWalkerT@users.noreply.github.com>
2018-11-14 00:11:14 +0900

    敵位置ファイル出力準備

---
+++ ShiningRoadProject/SourceCodo/EnemyFactory.cpp
@@ -0,0 +1,14 @@
+#include"EnemyFactory.h"
+
+const std::vector<clsCharactor*> clsEnemyFactory::CreateEnemy(std::string strFolderName)
+{
+
+}
+
+clsEnemyFactory::clsEnemyFactory()
+{
+}
+
+clsEnemyFactory::~clsEnemyFactory()
+{
+}
---
+++ ShiningRoadProject/SourceCodo/EnemyFactory.h
@@ -0,0 +1,35 @@
+#include"Global.h"
+#include"EnemyRobo.h"
+
+class clsEnemyFactory
+{
+public:
+	clsEnemyFactory();
+	~clsEnemyFactory();
+
+	const std::vector<clsCharactor*> CreateEnemy(std::string strFolderName);
+
+private:
+	enum enEnemyType
+	{
+		enEnemyTypeMinion = 0,
+		enEnemyTypeRobo,
+
+		enEnemyTypeSize
+	};
+
+	enum enEnemyDataFileOrder
+	{
+		enEnemyDataFileOrderEnemyType = 0,
+		enEnemyDataFileOrderEnemyNum,
+		enEnemyDataFileOrderPosX,
+		enEnemyDataFileOrderPosY,
+		enEnemyDataFileOrderPosZ,
+		enEnemyDataFileOrderPitch,
+		enEnemyDataFileOrderYaw,
+		enEnemyDataFileOrderRoll,
+
+		enEnemyDataFileOrderSize
+	};
+};
+
