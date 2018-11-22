771fb31db80e8427b2be387b77dd7fdb558c7374
Tone2387 <34734345+Tone2387@users.noreply.github.com>
2018-11-22 18:40:13 +0900

    ブレンドステートクラス完成( notシングルトン )

--- ShiningRoadProject/SourceCodo/BlendState.cpp
+++ ShiningRoadProject/SourceCodo/BlendState.cpp
@@ -3,133 +3,94 @@
 
 
 
-clsBLEND_STATE::clsBLEND_STATE(
-	ID3D11Device* const pDevice,
-	ID3D11DeviceContext* const pContext,
-	const bool isAlphaBlend )
-{
-	assert( pDevice );
-	assert( pContext );
-
-	//アルファブレンド用ブレンドステート作成.
-	//pngファイル内にアルファ情報があるので、透過するようにブレンドステートで設定する.
-	D3D11_BLEND_DESC blendDesc;
-	ZeroMemory( &blendDesc, sizeof( D3D11_BLEND_DESC ) );	//初期化.
-	blendDesc.IndependentBlendEnable = false;			//false:RenderTarget[0]のメンバーのみ使用する。true:RenderTarget[0～7]が使用できる(レンダーターゲット毎に独立したブレンド処理).
-	blendDesc.AlphaToCoverageEnable = false;			//true:アルファトゥカバレッジを使用する.
-
-	//表示タイプ
-	blendDesc.RenderTarget[0].BlendEnable = isAlphaBlend;					//true:アルファブレンドを使用する.
-	blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;		//アルファブレンドを指定.
-	blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;//アルファブレンドの反転を指定.
-	blendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;			//ADD：加算合成.
-	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;		//そのまま使用.
-	blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;		//何もしない.
-	blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;	//ADD：加算合成.
-	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;//全ての成分(RGBA)へのデータの格納を許可する.
 
 
-	ID3D11BlendState* pBlendState = nullptr;
-	if( FAILED( pDevice->CreateBlendState( &blendDesc, &pBlendState ) ) ){
-		assert( !"ブレンドステートの作成に失敗" );
-	}
-
-
-	const UINT mask = 0xffffffff;	//マスク値白.
-	//ブレンドステートの設定.
-	pContext->OMSetBlendState( pBlendState, NULL, mask );
-
-
-}
-
-
-/*
 clsBLEND_STATE::clsBLEND_STATE(
 	ID3D11Device* const pDevice,
 	ID3D11DeviceContext* const pContext )
-	:m_pContext( pContext )
-	,m_isAlpha( false )
+	:m_wpDevice( pDevice )
+	,m_wpContext( pContext )
+	,m_encBrendFlg( encBLEND_FLAG::NOT_ALPHA )
 {
-	for( unsigned char i=0; i<enBLEND_STATE_size; i++ ){
-		m_pBlendState[i] = nullptr;
-	}
-
-	if( FAILED( CreateBlendState( pDevice ) ) ){
-		assert( !"Can't Create Blend State" );
-	}
-
-	SetBlend( !m_isAlpha );
+	//初期化と別のものを置かないと作らない.
+	SetBlend( encBLEND_FLAG::ALPHA );
 }
 
 clsBLEND_STATE::~clsBLEND_STATE()
 {
-	m_isAlpha = false;
-
-	for( unsigned char i=0; i<enBLEND_STATE_size; i++ ){
-		SAFE_RELEASE( m_pBlendState[i] );
-	}
-
-	m_pContext = nullptr;
+	m_wpDevice = nullptr;
+	m_wpContext = nullptr;
 }
 
 
-//ブレンドステート作成.
-HRESULT clsBLEND_STATE::CreateBlendState( ID3D11Device* const pDevice )
+
+void clsBLEND_STATE::SetBlend( const encBLEND_FLAG encBlendFlag )
 {
-	assert( pDevice );
+	//今と同じならわざわざやらんでよい.
+	if( m_encBrendFlg == encBlendFlag ){
+		return;
+	}
+	m_encBrendFlg = encBlendFlag;
+
+	ID3D11BlendState* pBlendState = nullptr;
+	if( FAILED( CreateBlendState( &pBlendState ) ) ){
+		assert( !"Can't Create Blend State" );
+	}
+
+	//ブレンドステートの設定.
+	const UINT mask = 0xffffffff;	//マスク値白.
+	m_wpContext->OMSetBlendState( pBlendState, NULL, mask );
+
+	SAFE_DELETE( pBlendState );
+}
+
+//ブレンドステート作成.
+HRESULT clsBLEND_STATE::CreateBlendState( ID3D11BlendState** ppBlendState )
+{
+	D3D11_BLEND_DESC blendDesc;
+	CreateBlendDesc( &blendDesc );
 
 	//アルファブレンド用ブレンドステート作成.
 	//pngファイル内にアルファ情報があるので、透過するようにブレンドステートで設定する.
-	D3D11_BLEND_DESC blendDesc;
-	ZeroMemory( &blendDesc, sizeof( D3D11_BLEND_DESC ) );	//初期化.
-	blendDesc.IndependentBlendEnable = false;			//false:RenderTarget[0]のメンバーのみ使用する。true:RenderTarget[0～7]が使用できる(レンダーターゲット毎に独立したブレンド処理).
-	blendDesc.AlphaToCoverageEnable = false;			//true:アルファトゥカバレッジを使用する.
-
-	//表示タイプ
-//	blendDesc.RenderTarget[0].BlendEnable = true;					//true:アルファブレンドを使用する.
-	blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;		//アルファブレンドを指定.
-	blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;//アルファブレンドの反転を指定.
-	blendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;			//ADD：加算合成.
-	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;		//そのまま使用.
-	blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;		//何もしない.
-	blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;	//ADD：加算合成.
-	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;//全ての成分(RGBA)へのデータの格納を許可する.
-
-	bool tmpBlendEnable[ enBLEND_STATE_size ];
-	tmpBlendEnable[ enBLEND_STATE_ALPHA_ON ] = true;
-	tmpBlendEnable[ enBLEND_STATE_ALPHA_OFF ] = false;
-
-	for( unsigned char i=0; i<enBLEND_STATE_size; i++ )
-	{
-		assert( !m_pBlendState[i] );
-		blendDesc.RenderTarget[0].BlendEnable = tmpBlendEnable[i];
-		if( FAILED( pDevice->CreateBlendState( &blendDesc, &m_pBlendState[i] ) ) ){
-			assert( !"ブレンドステートの作成に失敗" );
-			return E_FAIL;
-		}
+	if( FAILED( m_wpDevice->CreateBlendState( &blendDesc, ppBlendState ) ) ){
+		assert( !"ブレンドステートの作成に失敗" );
+		return E_FAIL;
 	}
 
 	return S_OK;
 }
 
-
-void clsBLEND_STATE::SetBlend( const bool isAlpha ) const
+//ブレンドデスク作成.
+D3D11_BLEND_DESC clsBLEND_STATE::CreateBlendDesc( D3D11_BLEND_DESC* const pBlendDesc )
 {
-	//今と同じならわざわざやらんでよい.
-	if( isAlpha == m_isAlpha ){
-		return;
+	assert( pBlendDesc );
+
+	ZeroMemory( pBlendDesc, sizeof( D3D11_BLEND_DESC ) );	//初期化.
+	pBlendDesc->IndependentBlendEnable	= false;			//false:RenderTarget[0]のメンバーのみ使用する。true:RenderTarget[0～7]が使用できる(レンダーターゲット毎に独立したブレンド処理).
+	pBlendDesc->AlphaToCoverageEnable	= false;			//true:アルファトゥカバレッジを使用する.
+
+	//表示タイプ
+	pBlendDesc->RenderTarget[0].BlendEnable				= true;							//true:アルファブレンドを使用する.
+	pBlendDesc->RenderTarget[0].SrcBlend				= D3D11_BLEND_SRC_ALPHA;		//アルファブレンドを指定.
+	pBlendDesc->RenderTarget[0].DestBlend				= D3D11_BLEND_INV_SRC_ALPHA;	//アルファブレンドの反転を指定.
+	pBlendDesc->RenderTarget[0].BlendOp					= D3D11_BLEND_OP_ADD;			//ADD：加算合成.
+	pBlendDesc->RenderTarget[0].SrcBlendAlpha			= D3D11_BLEND_ONE;				//そのまま使用.
+	pBlendDesc->RenderTarget[0].DestBlendAlpha			= D3D11_BLEND_ZERO;				//何もしない.
+	pBlendDesc->RenderTarget[0].BlendOpAlpha			= D3D11_BLEND_OP_ADD;			//ADD：加算合成.
+	pBlendDesc->RenderTarget[0].RenderTargetWriteMask	= D3D11_COLOR_WRITE_ENABLE_ALL;	//全ての成分(RGBA)へのデータの格納を許可する.
+
+
+	switch( m_encBrendFlg )
+	{
+	case encBLEND_FLAG::ALPHA:
+		pBlendDesc->RenderTarget[0].BlendEnable = static_cast<BOOL>( true );
+		break;
+	case encBLEND_FLAG::NOT_ALPHA:
+		pBlendDesc->RenderTarget[0].BlendEnable = static_cast<BOOL>( false );
+		break;
+	default:
+		break;
 	}
 
-	const UINT mask = 0xffffffff;	//マスク値白.
-
-	assert( m_pContext );
-	//ブレンドステートの設定.
-	if( isAlpha ){		
-		m_pContext->OMSetBlendState( m_pBlendState[ enBLEND_STATE_ALPHA_ON ], NULL, mask );
-	}
-	else{
-		m_pContext->OMSetBlendState( m_pBlendState[ enBLEND_STATE_ALPHA_OFF ], NULL, mask );
-	}
+	return *pBlendDesc;
 }
-
-*/
--- ShiningRoadProject/SourceCodo/BlendState.h
+++ ShiningRoadProject/SourceCodo/BlendState.h
@@ -5,39 +5,38 @@
 
 
 //ID3D11BlendStateを簡単に操作するためのクラス.
-
-
-//class clsBLEND_STATE
-//{
-//public:
-//	clsBLEND_STATE(
-//		ID3D11Device* const pDevice,
-//		ID3D11DeviceContext* const pContext );
-//	~clsBLEND_STATE();
-//
-//	void SetBlend( const bool isAlpha ) const;
-//
-//private:
-//
-//	HRESULT CreateBlendState( ID3D11Device* const pDevice );
-//
-//	bool m_isAlpha;
-//
-//	ID3D11DeviceContext*	m_pContext;
-//
-//	ID3D11BlendState*	m_pBlendState[ enBLEND_STATE_size ];		//ブレンドステート.
-//
-//};
-
+//シングルトン.
 class clsBLEND_STATE
 {
 public:
+
 	clsBLEND_STATE(
 		ID3D11Device* const pDevice,
-		ID3D11DeviceContext* const pContext,
-		const bool isAlphaBlend );
+		ID3D11DeviceContext* const pContext );
+	~clsBLEND_STATE();
 
+	enum class encBLEND_FLAG : UCHAR
+	{
+		ALPHA = 0,
+		NOT_ALPHA,		
+	};
 
+	void SetBlend( const encBLEND_FLAG encBlendFlag );
+
+private:
+
+	//ブレンドステート作成.
+	HRESULT CreateBlendState( ID3D11BlendState** ppBlendState );
+	
+	//ブレンドデスク作成.
+	D3D11_BLEND_DESC CreateBlendDesc( D3D11_BLEND_DESC* const pBlendDesc );
+
+private:
+
+	ID3D11Device*		 m_wpDevice;
+	ID3D11DeviceContext* m_wpContext;
+
+	encBLEND_FLAG m_encBrendFlg;
 };
 
 
