9b4ab338fd8098702e2b19b80dd2ec3aca7ac913
DreamWalkerT <39514978+DreamWalkerT@users.noreply.github.com>
2018-12-17 01:12:47 +0900

    修正

--- ShiningRoadProject/SourceCodo/SoundManagerBase.cpp
+++ ShiningRoadProject/SourceCodo/SoundManagerBase.cpp
@@ -336,7 +336,7 @@ bool clsSOUND_MANAGER_BASE::Play(
 
 	vpSound[ uiNo ][ viNum[ uiNo ] ]->SeekToStart();
 
-	return false;// vpSound[uiNo][viNum[uiNo]]->Play(bNotify);
+	return vpSound[uiNo][viNum[uiNo]]->Play(bNotify);
 #endif//#if 0
 }
 
